a33a2837f141938009b7b158dd91d5c73067cd27
Yoogesh <yoogesh1983@gmail.com>
2019-09-17 08:32:47 -0400

    update

--- ProducerAndConsumerUsingWaitAndNotify/src/com/yoogesh/multithreading/threads/Consumer.java
+++ ProducerAndConsumerUsingWaitAndNotify/src/com/yoogesh/multithreading/threads/Consumer.java
@@ -24,6 +24,13 @@ public class Consumer extends Thread {
 		// Assume there is 10000 lines of codes here
 	}
 
+	
+	/**
+	 * Theoritically we should not have this seperate methd and all lines should be inside the above synchronized block.
+	 * The reason behind this is because this method is not synchronized and hence can be called from other non-synchronized method too and 
+	 * that's where the problem occurs. But here, since it is only called from synchronized block, the problem not occured.
+	 * Also i have done this to show the clearness. but in real scenario, don't do it like this and always put inside synchronized block above directly.
+	 */
 	private void removeItem() {
 		System.out.println("Consumer aquired the lock.Going to remove the number from a queue now.");
 
