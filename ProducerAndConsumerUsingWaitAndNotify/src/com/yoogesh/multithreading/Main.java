9e84b36db6b477a3c9e717e2dfc2c78383179c83
Yoogesh <yoogesh1983@gmail.com>
2019-09-17 21:23:34 -0400

    Adding ExecutorService framework

--- ProducerAndConsumerUsingWaitAndNotify/src/com/yoogesh/multithreading/Main.java
+++ ProducerAndConsumerUsingWaitAndNotify/src/com/yoogesh/multithreading/Main.java
@@ -1,5 +1,8 @@
 package com.yoogesh.multithreading;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import com.yoogesh.multithreading.database.MyQueue;
 import com.yoogesh.multithreading.threads.Consumer;
 import com.yoogesh.multithreading.threads.Producer;
@@ -12,14 +15,29 @@ public static void main(String[] args) throws InterruptedException {
 	Producer t1 = new Producer(mq);
 	Consumer t2 = new Consumer(mq);
 	
+	/*
 	t1.start();
 	t2.start();
-	
 	
 	//Main thread should wait until t1 and t2 complete their task. Only after that, the program can be terminated
 	t1.join();
 	t2.join();
 	
 	System.out.println("Main thread finally completes the execution and going to terminate the program safely.");
+	*/
+		
+	Thread[] jobs = { t1, t2 };
+	ExecutorService service = Executors.newFixedThreadPool(2);
+	for(Thread job : jobs)
+	{
+      service.submit(job);
+    }
+	service.shutdown();
+	
+	//Main thread should wait until t1 and t2 complete their task. Only after that, the program can be terminated.Since Join doesn't work here, so using sleep() method
+    Thread.sleep(100);
+	
+	System.out.println("Main thread finally completes the execution and going to terminate the program safely.");
+
 }
 }
